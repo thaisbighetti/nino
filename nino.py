cb569f3224b179f03d38153ad9e2ac3873928ed9
thaisbighetti <thais.bighetti@hotmail.com>
2025-01-23 17:58:41 -0300

    edsrffw

--- nino.py
+++ nino.py
@@ -3,7 +3,6 @@ import aiohttp
 import asyncio
 
 async def fetch(session, url):
-    """Faz uma requisição assíncrona e retorna o status HTTP sem seguir redirecionamentos."""
     try:
         async with session.get(url) as response:
             return response.status
@@ -12,11 +11,6 @@ async def fetch(session, url):
         return None
 
 async def requests_to_url(base_url, wordlist, max_depth):
-    """
-    Faz requisições recursivas baseadas na lista de palavras.
-    Para cada palavra encontrada, combina com outras palavras do wordlist,
-    mas evita URLs repetitivas ou profundidade excessiva.
-    """
     async with aiohttp.ClientSession() as session:
         wordlist = [line.strip() for line in wordlist]
         arguments_found = set()  
@@ -49,7 +43,6 @@ async def requests_to_url(base_url, wordlist, max_depth):
     return arguments_found
 
 async def main():
-    """Ponto de entrada principal para o script."""
     parser = argparse.ArgumentParser(description='Send async requests to a URL with a list of words')
     parser.add_argument('--wordlist', type=argparse.FileType('r'), required=True, help='A txt file containing a list of words')
     parser.add_argument('--url', type=str, required=True, help='A URL to send requests to')
